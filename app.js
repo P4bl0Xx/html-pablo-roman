accfea3f6ae24c8972bf1568333b1b570d6641f8
P4bl0Xx <123569861+P4bl0Xx@users.noreply.github.com>
2023-02-08 10:25:00 -0500

    febrero 8

--- app.js
+++ app.js
@@ -2,7 +2,31 @@ document.addEventListener("DOOMContentLoaded", () => {
   const cardsAdj = [
     {
       name: "Trollface",
-      imagenes: "imagenes/TrollFace og.jpg"
+      imagenes: "magenes/TrollFace og.jpg"
+    },
+    {
+      name: "Trollface",
+      imag: "imagenes/trolface 2.jpge"
+    },
+    {
+      name: "Trollface",
+      img: "imagenes/trollface ayo 8.jpg"
+    },
+    {
+      name: "Trollface",
+      img: "imagenes/trollface cure 7.png"
+    },
+    {
+      name: "Trollface",
+      img: "imagenes/trollFace imp 4.jpg"
+    },
+    {
+      name: "Trollface",
+      img: "imagenes/trollFace lol 6.jpg"
+    },
+    {
+      name: "Trollface",
+      imagenes: "magenes/TrollFace og.jpg"
     },
     {
       name: "Trollface",
