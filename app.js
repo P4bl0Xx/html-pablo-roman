9d3c678ebc69160f48ab5cad2f6d2eb76ed1700e
P4bl0Xx <123569861+P4bl0Xx@users.noreply.github.com>
2023-03-15 10:18:25 -0500

    15/03/2023 uwu

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const cardsAdj = [
     {
       name: "Trollface",
@@ -49,36 +49,30 @@ document.addEventListener("DOOMContentLoaded", () => {
       img: "imagenes/trollFace lol 6.jpg"
     }
   ];
+
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasEscogidas = [];
+  var cartasEscogidasId = [];
+  var cartaGanadas = [];
+
+  function crearTablero() {
+    for (let i = 0; i < cardsAdj.length; i++) {
+      var carta = document.createElement("img");
+      carta.setAttribute("src", "imagenes/reverso.png");
+      carta.setAttribute("data-id", i);
+      carta.addEventListener("click", voltearCarta);
+      cuadricula.appendChild(carta);
+    }
+  }
+  function voltearCarta() {
+    var cardId = this.getAttribute("data-id");
+    cartasEscogidas.push(cardsAdj[cardId].name);
+    cartasEscogidasId.push(cardId);
+    this.setAttribute("src", cardsAdj[cardId].img);
+    if (cartasEscogidas.length === 2) {
+      setTimeout(verificarPareja, 1500);
+    }
+  }
+  crearTablero();
 });
-
-const cuadricula = document.querySelector(".cuadricula");
-const resultado = document.querySelector("#resultado");
-var cartasEscogidas = [];
-var cartasEscogidasId = [];
-var cartaGanadas = [];
-
-function crearTablero (){
-  for (let i = 0; i < cardAdj.length; i++) { // Loop que inicia en i=0, se incrementa de 1 en 1 (i++) y finaliza
-                                             // cuando i=cardAdj.length, es decir, 12.
-    var carta = document.createElement('img'); // Crea elemento img cada vez que se ejecuta la función.
-    carta.setAttribute('src', 'images/reverso.png'); //Asignar nuevo atributo a cada carta
-                                                     //igual a la imagen 'reverso'.
-    carta.setAttribute('data-id', i); //Asignar como atributo a cada carta creada, el id=i.
-    carta.addEventListener('click', voltearCarta); //Detectar si la carta asignada recibe un click y ejecuta 
-                                                   //la función voltearCarta.
-    cuadricula.appendChild(carta); // Relaciona la carta creada con la variable cuadricula enlazada con la clase
-                                   // cuadricula de html para que se imprima la carta creada en el div de html.
-  }
-} 
-function voltearCarta() {
-  var carId = this.getAttribute("data-id");
-  cartasEscogidas.push(carAdj[cardId].name)
-  cartasEscogidasId.push(cardId)
-  this.setAttribute("src", cardAdj[cardId].img)
-if (cartasEscogidas.length === 2) {
-  setTimeout(verificarPareja, 1000);
-  }
-}
-crearTablero();
-}); 
-
